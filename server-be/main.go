5ada70d519a9e2d4328aa8b0058df56aec39392c
Milos Gavranovic <mgavranovic7@gmail.com>
2025-12-30 20:48:02 +0100

    Add server scaffolding

---
+++ server-be/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"log"
+	"math/big"
+	"net"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/quic-go/quic-go/http3"
+)
+
+type Server struct {
+	cfg http3.Server
+}
+
+func main() {
+	// setup for logging
+	file, err := os.OpenFile("./logs/server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open the log file: %s", err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	// setup http3 server
+	mux := http.NewServeMux()
+	go func() {
+		log.Println("TCP HTTPS on :8443")
+		err := http.ListenAndServe(":8443", mux)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	tlsConfig := generateTLSConfig()
+	srv := &http3.Server{
+		Addr:      ":8443",
+		Handler:   mux,
+		TLSConfig: tlsConfig,
+	}
+
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("Testge"))
+	})
+
+	log.Printf("Server starting on port %s", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Printf("Error starting the server: %s", err)
+	}
+}
+
+func generateTLSConfig() *tls.Config {
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			CommonName: "localhost",
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(24 * time.Hour),
+
+		KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+
+		DNSNames: []string{"localhost"},
+		IPAddresses: []net.IP{
+			net.ParseIP("127.0.0.1"),
+		},
+	}
+
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+
+	cert, _ := tls.X509KeyPair(certPEM, keyPEM)
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		MinVersion:   tls.VersionTLS13,
+		NextProtos:   []string{"h3"},
+	}
+}
