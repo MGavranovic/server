5b75bc7f25376f1ad76fcf40f5f557a37be2826f
Milos Gavranovic <mgavranovic7@gmail.com>
2025-12-30 14:20:32 +0100

    Initial commit

---
+++ server-fe/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p className="font-bold underline">
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
